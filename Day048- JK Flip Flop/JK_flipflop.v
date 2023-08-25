3f9c6a6ecea86408da9e56dd101f0050440e98bc
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-08-25 23:45:07 +0530

    Update JK_flipflop.v
    Looking at the code I am guessing you used vivado. The RTL design code's hardware realization seems incorrect in vivado Elaborate Design. Your code is creating 2 muxs instead of 1 as the code and the hardware for JK flipflop suggests. This patch clears that issue, get back to me for more information regarding this.

--- Day048- JK Flip Flop/JK_flipflop.v
+++ Day048- JK Flip Flop/JK_flipflop.v
@@ -1,21 +1,35 @@
-`timescale 1ns / 1ps
-
-module JK_flipflop(
-    input j,k,clk,reset,
-    output reg Q
+module JK_flipflop (
+    input j, k, clk, rst, 
+    output reg q, qbar
     );
-    always@(posedge clk)
-          begin
-            if({reset})
-            Q <= 1'b0;
-            else
-                begin
-                case({j,k})
-                2'b00:Q<=Q;
-                2'b01:Q<=1'b0;
-                2'b10:Q<=1'b1;
-                2'b11:Q<=~Q;
-                endcase
-                end          
-         end
+
+
+parameter HOLD = 2'b00, SET = 2'b10, RESET = 2'b01, TOGGLE = 2'b11;
+reg q1, q1bar;
+
+always@(*)
+begin
+    case({j,k})
+    00: q1=q;
+    01: q1=1'b0;
+    10: q1=1'b1;
+    11: q1=~q;
+    default: q1=1'bz;
+    endcase 
+    q1bar=~q1;
+end
+
+always@(posedge clk)
+begin
+    if(rst)begin
+    q<=1'b0;
+    qbar<=1'b1;
+    end
+    
+    else begin
+    q<=q1;
+    qbar<=q1bar;
+    end
+end
+
 endmodule
